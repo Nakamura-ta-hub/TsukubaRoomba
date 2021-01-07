e977851bf1f4173c823e0520a02a2557026d469b
nakamurata <57085424+Nakamura-ta-hub@users.noreply.github.com>
2021-01-07 18:42:08 +0900

    Update 5-10_send_uart_m5stickv_maixpy.py

--- 5-10_send_uart_m5stickv_maixpy.py
+++ 5-10_send_uart_m5stickv_maixpy.py
@@ -21,3 +21,33 @@ while True:
 
 uart_Port.deinit()
 del uart_Port
+
+
+または
+
+
+from machine import UART
+from board import board_info
+from fpioa_manager import fm
+from Maix import GPIO
+import sensor, lcd
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_windowing((160,160))
+sensor.set_vflip(1)
+sensor.set_hmirror(1)
+sensor.run(1)
+sensor.skip_frames()
+
+
+fm.register(35, fm.fpioa.UART1_TX, force=True)
+fm.register(34, fm.fpioa.UART1_RX, force=True)
+
+uart = UART(UART.UART1, 921600,8,0,0, timeout=1000, read_buf_len=4096)
+
+
+while(True):
+    img = sensor.snapshot()
+    uart.write(img)
